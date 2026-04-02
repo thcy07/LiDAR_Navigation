ac8fe219847c742126ad00bd30d87af464a340b4
Bryce-Student-16 <jen24023@byui.edu>
2026-04-02 12:30:03 -0600

    Buzzer and lidar working together

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,10 +41,15 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
+float PWM_Duty_Cycle;
+int PWM_Period = 1000;
+
 // TF-Luna receive buffer (1 byte at a time)
 uint8_t tf_rx_byte;
 
@@ -53,7 +58,7 @@ uint8_t tf_frame[9];
 uint8_t tf_index = 0;
 
 // Parsed values
-uint16_t tf_distance_cm = 0;
+uint16_t tf_distance_cm;
 uint16_t tf_strength = 0;
 float tf_temperature = 0;
 
@@ -64,12 +69,14 @@ char msg[100];
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
+static void MX_TIM1_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 void TF_Luna_Start(void);
 void TF_Luna_Process(uint8_t byte);
 void UART_Print(char *str);
+void Vibration_Small(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -131,13 +138,113 @@ void TF_Luna_Process(uint8_t byte)
             snprintf(msg, sizeof(msg),
                      "Distance: %d cm | Strength: %d | Temp: %.2f C\r\n",
                      tf_distance_cm, tf_strength, tf_temperature);
-
             UART_Print(msg);
         }
 
         tf_index = 0;
     }
 }
+
+void Vibration_Toggle()
+{
+
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+
+	// Set duty cycle (50%)
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+
+	HAL_Delay(2000);
+
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	__HAL_TIM_MOE_DISABLE(&htim1);
+}
+
+
+void Vibration_Three_Buzz()
+{
+	//Starts PWM and associated timer
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+	//Set duty cycle (100%)
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+
+	HAL_Delay(240);
+
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	__HAL_TIM_MOE_DISABLE(&htim1);
+
+	HAL_Delay(50);
+
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+	//Set duty cycle (100%)
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1000);
+
+	HAL_Delay(240);
+
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	__HAL_TIM_MOE_DISABLE(&htim1);
+
+	HAL_Delay(50);
+
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+	//Set duty cycle (100%)
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1000);
+
+	HAL_Delay(240);
+
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	__HAL_TIM_MOE_DISABLE(&htim1);
+
+}
+
+
+void Vibration_Maximum()
+{
+
+	//Set duty cycle (100%)
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1000);
+
+	HAL_Delay(20);
+
+	__HAL_TIM_MOE_DISABLE(&htim1);
+}
+
+
+void Vibration_Small()
+{
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+	//Set duty cycle (43%)
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 430);
+
+	HAL_Delay(20);
+
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	__HAL_TIM_MOE_DISABLE(&htim1);
+}
+
+
+void Vibration_Two_Tone()
+{
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+	//Set duty cycle (50%)
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+
+	HAL_Delay(1000);
+
+	//Set duty cycle (100%)
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 1000);
+
+	HAL_Delay(1000);
+
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+	__HAL_TIM_MOE_DISABLE(&htim1);
+}
+
 /* USER CODE END 0 */
 
 /**
@@ -169,22 +276,38 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_USART2_UART_Init();
+  MX_TIM1_Init();
   MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
   UART_Print("TF-Luna starting...\r\n");
   TF_Luna_Start();
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+		  if (tf_distance_cm < 200)
+		  	      {
+			  PWM_Duty_Cycle = (1 - ((float)tf_distance_cm)/200)*0.65 + 0.35;
+				__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (int)(PWM_Duty_Cycle * PWM_Period));
+
+				HAL_Delay(20);
+
+				__HAL_TIM_MOE_DISABLE(&htim1);
+		  	      }
+		  else{
+			  PWM_Duty_Cycle = 0;
+			  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, (int)(PWM_Duty_Cycle * PWM_Period));
+			  HAL_Delay(20);
+			  __HAL_TIM_MOE_DISABLE(&htim1);
+
+		  }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-//	  UART_Print("HELLO\r\n");
-	  HAL_Delay(1000);
   }
   /* USER CODE END 3 */
 }
@@ -236,6 +359,76 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 79;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 999;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.BreakFilter = 0;
+  sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+  sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+  sBreakDeadTimeConfig.Break2Filter = 0;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
 }
 
 /**
